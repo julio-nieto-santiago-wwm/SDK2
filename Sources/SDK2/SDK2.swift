978ed55c3c845f63ae0b99afd981b5cec3eecfd5
Julio Nieto Santiago <julio.nieto@worldwidemobility.io>
2023-09-21 00:06:08 +0200

    Initial Commit

---
+++ Sources/SDK2/SDK2.swift
@@ -0,0 +1,6 @@
+public struct SDK2 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SDK2Tests/SDK2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SDK2
+
+final class SDK2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SDK2().text, "Hello, World!")
+    }
+}
